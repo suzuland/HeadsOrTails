6de5a7134694ac7951b5499adf235f9906fc8a9b
suzuland <riku68230@gmail.com>
2024-07-04 22:21:37 +0900

    Final revision
    Final revision

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -28,9 +28,9 @@ public class HeadsOrTails {
         System.out.println("Heads: " + heads + ", Tails: " + tails);
 
         if (heads > tails) {
-            System.out.println("You won!");
+            System.out.println(name + " won!");
         } else {
-            System.out.println("You lost!");
+            System.out.println(name + " lost!");
         }
     }
 }
